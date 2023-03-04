86a0d036bf1d1e547bb8fac541cc666b07f451c1
vagdevikatepalli <88127788+vagdevikatepalli@users.noreply.github.com>
2023-03-04 19:37:51 +0530

    Add files via upload

---
+++ chart.js
@@ -0,0 +1,29 @@
+
+const chart = document.querySelector(".chart");
+const canvas = document.createElement("canvas");
+canvas.width = 50;
+canvas.height = 50;
+chart.appendChild(canvas);
+
+const ctx = canvas.getContext("2d");
+
+ctx.lineWidth = 8;
+
+const R = 20;
+
+function drawCircle(color, ratio, anticlockwise){
+
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    ctx.arc( canvas.width/2, canvas.height/2, R, 0, ratio * 2 * Math.PI, anticlockwise);
+    ctx.stroke();
+}
+
+function updateChart( income, outcome){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    let ratio = income / (income+outcome);
+
+    drawCircle("#008000", - ratio, true);
+    drawCircle("#e91010", 1 - ratio, false);
+}
